635ef3288ac6624c726dc9ee7d0fda99f7b8781a
Angus Ng <144841298+angus-ng@users.noreply.github.com>
2024-03-31 22:36:04 -0700

    add peer review comments

--- Annabelle/validateUsername.js
+++ Annabelle/validateUsername.js
@@ -1,5 +1,5 @@
-function validateUsername(username) {
-    const validLength = username.length >= 6;
+function validateUsername(username) {        //angus: we would likely also check if a username already exists 
+    const validLength = username.length >= 6;    //angus: maybe in actual implementation 6 character minimum is too long
     let hasLetter = /[a-zA-z]/g.test(username);
     let hasNumber = /[0-9]/g.test(username);
 
